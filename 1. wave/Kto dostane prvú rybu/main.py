a072554986d7e141dd1e90785f17820dc0bd612b
Juriij <juro.kopiciar@gmail.com>
2024-11-02 21:29:24 +0100

    working on the main loop
    Main loop moves the penguins and check if they have visited the marketplace (LOL)

--- 1. wave/Kto dostane prvú rybu/main.py
+++ 1. wave/Kto dostane prvú rybu/main.py
@@ -1,16 +1,16 @@
 from typing import List
 
 
-def pop_visited_queries(visited_queries_iter, queries_dict):
-    for query in visited_queries_iter:   # O(q) after all it will run "q" times
-        queries_dict.pop(query)
+def pop_visited_queries(visited_queries, queries_dict):
+    for query in visited_queries:   # O(q) after all it will run "q" times
+        queries_dict.pop(query)     # O(1)
 
 
 def sort_penguins(first_penguins_temp):
     # Sorting penguin indexes
     for key in first_penguins_temp:        # O(q) after all it will run "q" times
         if len(first_penguins_temp[key]) != 1:
-            first_penguins_temp[key].sort()
+            first_penguins_temp[key].sort()     
 
 
 
@@ -19,14 +19,66 @@ def sort_penguins(first_penguins_temp):
 
 def move_penguins(init_run, positions, velocities, queries_dict, first_penguins_temp):
     
-    visited_queries_iter = set()  # variable that remembers queries visited for each iteration {query_value} 
+    visited_queries = dict()     # {query: {shortest_time: [penguins' indexes]}}
+    
+    if init_run:
+        # This loop checks if there are penguins located at all the marketplace's possible positions
+        for i in range(len(positions)):   # O(n)
+            if positions[i] in queries_dict:  
+                penguin_index = i
+                visited_query = positions[i]
+
+                if visited_query not in visited_queries:
+                    visited_queries[visited_query] = {0: []}
+                
+                visited_queries[visited_query][0].append(penguin_index)
+
+
+    else:
+        for i in range(len(positions)):  # iterates over all the penguins O(n)
+            start = positions[i]
+            end = start + velocities[i]
+
+            for j, step_ahead in enumerate(range(start, end+1)):  # moves a penguin step by step in both directions
+                step_back = start - j - 1
+    
+                # has the penguin arrived to a marketplace?
+                if step_ahead in queries_dict:   # forward motion     
+                    time = (step_ahead - start) / velocities[i]    # time <= 1sec
+                    
+                    if step_ahead not in visited_queries:
+                        visited_queries[step_ahead] = {time: [i]}
+                    
+                    else:
+                        if visited_queries[step_ahead] == time:
+                            visited_queries[step_ahead][time].append(i)
+                        
+                        elif visited_queries[step_ahead] > time:
+                            visited_queries[step_ahead] = {time: [i]}
+                
+
+                if step_back in queries_dict:     # backward motion
+                    time = (start - step_back) / velocities[i]   # time <= 1sec
+
+                else:
+                    if visited_queries[step_back] == time:
+                        visited_queries[step_back][time].append(i)
+                    
+                    elif visited_queries[step_back] > time:
+                        visited_queries[step_back] = {time: [i]}
+
+
+        
+
+            positions[i] = end       # update penguin's position after he finished his move
 
 
 
 
 
 
-    pop_visited_queries(visited_queries_iter, queries_dict)
+
+    pop_visited_queries(visited_queries, queries_dict)
 
 
 
@@ -40,7 +92,8 @@ def solve(positions: List[int], velocities: List[int], queries: List[int]) -> Li
     first_penguins = []  # [[position-indexes]]        
     first_penguins_temp = {}  # {query-index: [position-indexes]}
     
-    queries_dict = {value: index for index, value in enumerate(queries)} # {query:index} O(q)
+    queries_dict = {value: index for index, value in enumerate(queries)}            #{query:index} O(q)
+    queries_dict_reversed = {index: value for value, index in queries_dict.items()} #{index:query} O(q)
     
     init_run = True
 
