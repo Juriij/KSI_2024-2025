d2fc53c73ee68cb4f59be0b0364e31c3e010b470
Juriij <juro.kopiciar@gmail.com>
2024-11-06 21:17:44 +0100

    removing testing lines

--- 1. wave/Kto dostane prvú rybu/main.py
+++ 1. wave/Kto dostane prvú rybu/main.py
@@ -1,6 +1,4 @@
 from typing import List
-from random import seed, randint
-import time
 
 # time complexity: O(q*n)
 def solve(positions: List[int], velocities: List[int], queries: List[int]) -> List[List[int]]:
@@ -23,30 +21,4 @@ def solve(positions: List[int], velocities: List[int], queries: List[int]) -> Li
         best_candidates.sort()
         first_penguins.append(best_candidates)
             
-            
-
     return first_penguins
-
-
-
-
-
-
-# # Toto je príklad pre prvú sadu -- ku každej pozícii pribehne ako prvý iba jediný tučniak
-# positions = [28, 95, 0, 98] 
-# velocities = [1, 8, 2, 10] 
-# queries = [61, 26, 40, 34, 84, 85] 
-# print(solve(positions, velocities, queries))
-# # [[3], [0], [3], [0], [1], [1]]
-
-
-
-
-
-
-# # Toto je príklad pre druhú sadu -- všetky pozície tučniakov sú naľavo od všetkých možností miesta stánku
-# positions = [44, 12, 30]
-# velocities = [2, 9, 6]
-# queries = [77, 78, 59, 66, 61, 80]
-# print(solve(positions, velocities, queries))
-# # [[1], [1], [2], [1, 2], [2], [1]]
