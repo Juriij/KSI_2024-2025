0bf0ae81fa78c49321c91346f5f5fe4018c84ce8
Juriij <juro.kopiciar@gmail.com>
2024-11-06 20:10:24 +0100

    O(q*n) approach

--- 1. wave/Kto dostane prvú rybu/main.py
+++ 1. wave/Kto dostane prvú rybu/main.py
@@ -1,15 +1,31 @@
 from typing import List
+from random import seed, randint
+import time
 
-
+# time complexity: O(q*n)
 def solve(positions: List[int], velocities: List[int], queries: List[int]) -> List[List[int]]:
-    first_penguins = []  # [[position-indexes]]        
+    first_penguins = []  # [[position-indexes]]       
 
+    for i in range(len(queries)):
+        best_candidates = []
+        best_time = 10**20
+
+        for j in range(len(positions)):
+            time = abs(queries[i] - positions[j]) / velocities[j]
+
+            if time == best_time:
+                best_candidates.append(j)
+
+            elif time < best_time:
+                best_candidates = [j]
+                best_time = time
+
+        best_candidates.sort()
+        first_penguins.append(best_candidates)
+            
+            
 
     return first_penguins
-
-
-
-
 
 
 
