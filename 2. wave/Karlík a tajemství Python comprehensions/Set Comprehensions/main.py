a60dad47edce89011f97f61111083b82c388cc31
Juriij <juro.kopiciar@gmail.com>
2024-12-04 20:49:11 +0100

    set comprehension in action

---
+++ 2. wave/Karlík a tajemství Python comprehensions/Set Comprehensions/main.py
@@ -0,0 +1,21 @@
+# Vytvoř set comprehension, který z textu získá unikátní samohlásky
+# (Pro lidi co nikdy neslyšeli, že 'y' je samohláska - tak jako já. Tak je...)
+# Takze pro "Karlik je COOL", vratí:
+# {
+#   a, e, i, o
+# }
+
+def unique_vowels(s: str)-> set[str]:
+    return {letter.lower() for letter in s  if letter.lower() in {"a", "i", "u", "e", "o", "y"}}
+
+print(unique_vowels("Karlik je COOL") )
+
+
+assert unique_vowels("Karlik je COOL") == {'a', 'e', 'i', 'o'}, "Test case 1 failed"
+
+assert unique_vowels("hello world") == {'e', 'o'}, "Test case 2 failed"
+
+assert unique_vowels("AEIOUaeiou") == {'a', 'e', 'i', 'o', 'u'}, "Test case 3 failed"
+
+assert unique_vowels("bcdfghjklmnpqrstvwxyz") == {'y'}, "Test case 4 failed"
+
