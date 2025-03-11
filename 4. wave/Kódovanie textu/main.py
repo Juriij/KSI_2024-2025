b86e160fccd862839bb98d6a4e478215506d51a5
Juriij <juro.kopiciar@gmail.com>
2025-03-11 17:02:24 +0100

    Updating the encoder

--- 4. wave/Kódovanie textu/main.py
+++ 4. wave/Kódovanie textu/main.py
@@ -1,96 +1,42 @@
 from typing import List
-
-
-def convert_to_binary(num: int) -> str:
-    binary = ""
-    while True:
-        remainder = num % 2
-        binary += str(remainder)
-
-        if num // 2 == 0:
-            break 
-
-        num = num // 2
-
-    return binary[::-1]
-
-
+   
 # Urob Túto funkciu
 def encode_karlik(code_point: int) -> List[int]:
     bytes = []
-    binary_code = convert_to_binary(code_point)
 
-    b = len(binary_code) // 7
+    binary_code = bin(code_point)[2:]
+    binary_length = len(binary_code)
 
-    amount_remaining_bits = len(binary_code) - b*7
-
-
-    if amount_remaining_bits <= (8 - b+2):  # no need for extra byte
-        for i in range(b+1):
-            byte = ""
-
-            if i == 0:
-                byte = "0" + binary_code[:(8-(b+2))]
-                length_byte = len(byte)
-                binary_code = binary_code[(8-(b+2)):]
-
-                print(f'this is first preprocessed byte: {byte}')
-
-                for j in range(8 - length_byte):
-                    byte = "1" + byte
-
-                print(f'this is first byte: {byte}')
-
-
-            else:
-                byte += "0"
-                byte += binary_code[:7]
-                binary_code = binary_code[7:]
-
-
-            decimal = int(byte, base=2)
-            bytes.append(decimal)
-
-
-    else:  # we need extra byte
-        for i in range(b+2):
-            byte = ""
-
-            if i == 0:
-                byte = "0" + binary_code[:(8-(b+2))]
-                length_byte = len(byte)
-                binary_code = binary_code[(8-(b+2)):]
-
-                # print(f'this is first preprocessed byte: {byte}')
-                
-                for j in range(8 - length_byte):
-                    byte = "1" + byte
-
-                # print(f'this is first byte: {byte}')
-
-            else:
-                byte += "0"
-                byte += binary_code[:7]
-                binary_code = binary_code[7:]
-
-
-            decimal = int(byte, base=2)
-            bytes.append(decimal)
-
-
-    return bytes
-
-
+    amnt_data_bytes = binary_length // 7
 
     
+    amnt_remaining_bits = binary_length - (amnt_data_bytes * 7)
+
+    byte_head_length = amnt_data_bytes + 1 + 1
+    
+
+    for _ in range(amnt_data_bytes):
+        byte = "0" + binary_code[-7:]
+        binary_code = binary_code[:-7]
+
+        bytes.append(byte)
 
 
+    # head-first byte
+    byte = ""
+    for _ in range(byte_head_length-1):
+        byte += "1"
+    byte += "0"
 
 
+    for _ in range(8 - (byte_head_length + amnt_remaining_bits)):
+        byte += "0"
 
-print(encode_karlik(95))  # [192, 95]
-print(" ".join(bin(b)[2:].rjust(8, "0") for b in encode_karlik(95)))  # 11000000 01011111
-# print(encode_karlik(33))  # [161]
-# print(" ".join(bin(b)[2:].rjust(8, "0") for b in encode_karlik(33)))  # 10100001
-# print(encode_karlik(256))  # [194, 0]
-# print(" ".join(bin(b)[2:].rjust(8, "0") for b in encode_karlik(256)))  # 11000010 00000000
+    byte += binary_code
+    bytes.append(byte)
+
+
+    for i in range(len(bytes)):
+        bytes[i] = int(bytes[i], 2)
+
+    return bytes[::-1]
