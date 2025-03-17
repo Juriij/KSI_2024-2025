07ba4984cf730b18b84dd5d42881da31393cc535
Juriij <juro.kopiciar@gmail.com>
2025-03-17 22:27:26 +0100

    completing fibonacci sequence function

--- 4. wave/Funkce/Fibonacci/main.asm
+++ 4. wave/Funkce/Fibonacci/main.asm
@@ -3,18 +3,45 @@ segment code
         MOV SS, BX
         MOV SP, top
 
-        CALL fibonacci
+        POP DX         ; first_value
+        POP CX         ; second_value
+        POP AX         ; n_value
+
+        PUSH CX        ; F_0
+        PUSH DX        ; F_1
+
+        JMP fibonacci
         JMP end_program
 
 
 fibonacci:
-    
+    CMP AX, 0
+    JZ end_program_0
+
+    CMP AX, 1
+    JZ end_program
+    DEC AX
+
+    ADD CX, DX
+    PUSH CX
+
+    POP DX
+    POP CX
+    PUSH CX
+    PUSH DX
+
+    JMP fibonacci
 
 end_program:
     HLT
 
+end_program_0:
+    POP CX
+    HLT
+
+
 segment stack
         resb 65530
-top:    dw 1        ; F_1
-        dw 0        ; F_0
-        dw 3        ; n
+top:    dw 8        ; F_1
+        dw 4        ; F_0
+        dw 5        ; n
