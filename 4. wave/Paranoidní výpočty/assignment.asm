ad58bf199218290d403a4c50f6174636648fe4b9
Juriij <juro.kopiciar@gmail.com>
2025-03-23 21:16:37 +0100

    add and minus in assembly

--- 4. wave/Paranoidní výpočty/assignment.asm
+++ 4. wave/Paranoidní výpočty/assignment.asm
@@ -1,4 +1,3 @@
-
 segment code
 ..start MOV BX, data	
     	MOV DS, BX
@@ -9,12 +8,48 @@ segment code
 
 
 	MOV AX, 38
-	MOV BX, 28
 	PUSH AX
+
+	MOV BX, 28
 	PUSH BX
 
 	CALL minus
 	HLT
+
+
+
+
+
+; result in AX register	
+plus:
+      	POP AX     ; return address
+	POP BX     ; second_num
+	POP CX     ; first_num
+
+ 
+	CMP BX, 0
+	JS neg_and_aux_minus
+
+
+	JMP aux_plus
+
+
+aux_plus:
+	CMP BX, 0
+	JZ end_operation	
+	
+	INC CX
+	DEC BX
+	
+	JMP aux_plus
+
+
+
+neg_and_aux_minus:
+	NEG BX
+	JMP aux_minus	
+
+
 
 
 
@@ -26,13 +61,18 @@ minus:
       	POP AX     ; return address
 	POP BX     ; second_num
 	POP CX     ; first_num
+	
+ 
+	CMP BX, 0
+	JS neg_and_aux_plus
+
 
 	JMP aux_minus
 
 	
 aux_minus:
 	CMP BX, 0
-	JZ end_minus 	
+	JZ end_operation	
 	
 	DEC CX
 	DEC BX
@@ -41,7 +81,16 @@ aux_minus:
 		
 
 
-end_minus:
+neg_and_aux_plus:
+	NEG BX
+	JMP aux_plus
+
+
+
+
+
+
+end_operation:
 	PUSH AX
 	MOV AX, CX
 	
